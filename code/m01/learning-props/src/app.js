517a889781c961a124ddc43fd7d90c05f8b8f9c3
rhomennik <rhomerios2@gmail.com>
2019-03-25 09:14:30 -0400

    getDefaultProps

--- code/m01/learning-props/src/app.js
+++ code/m01/learning-props/src/app.js
@@ -6,12 +6,11 @@ import React from 'react'
 import Title from './title'
 
 const App = React.createClass({
+
     render: function() {
         return ( 
         <div className='1'>
-            <Title name='Rhomennik Firmino' / >
-            <label htmlFor='input' data-label='Label'>Input</label>
-            <input type="text" id='input' aria-hidden={true} />
+            <Title name="Rhomennik" lastname={{a: 'Ri', b: 'os' }} / >
         </div>
         )
     }
--- code/m01/learning-props/src/title.js
+++ code/m01/learning-props/src/title.js
@@ -3,9 +3,19 @@
 import React from 'react'
 
 const Title = React.createClass({
+   // Metodo default se nao tiver passado a props
+    getDefaultProps: function() {
+        return {
+            name: 'Desconhecido',
+            lastname: {
+                a: 'Vaz',
+                b: 'io'                
+            }
+        }
+    },
 render: function() {
     return(
-        <h1>Oi {this.props.name}</h1>
+        <h1>Oi {this.props.name + ' ' + this.props.lastname.a + this.props.lastname.b} </h1>
     )
 }
 })

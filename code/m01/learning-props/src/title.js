cad45862dc78670b866cfb116ca4722f326f5081
rhomennik <rhomerios2@gmail.com>
2019-03-25 09:35:09 -0400

    Renderizando com Funcoes pura

--- code/m01/learning-props/src/title.js
+++ code/m01/learning-props/src/title.js
@@ -2,22 +2,36 @@
 
 import React from 'react'
 
-const Title = React.createClass({
-   // Metodo default se nao tiver passado a props
-    getDefaultProps: function() {
-        return {
-            name: 'Desconhecido',
-            lastname: {
-                a: 'Vaz',
-                b: 'io'                
-            }
-        }
-    },
-render: function() {
-    return(
-        <h1>Oi {this.props.name + ' ' + this.props.lastname.a + this.props.lastname.b} </h1>
-    )
-}
-})
+const Title = ({name, lastname}) => (
 
+<h1>Ola {`${name} ${lastname.a}${lastname.b} !`}</h1>
+
+) 
+
+Title.defaultProps = {
+    name: 'Desconhecido',
+           lastname: {
+               a: 'Vaz',
+               b: 'io'                
+           }
+       }
+
+// const Title = React.createClass({
+// // Metodo default se nao tiver passado a props
+// getDefaultProps: function() {
+//         return {
+//             name: 'Desconhecido',
+//             lastname: {
+//                 a: 'Vaz',
+//                 b: 'io'                
+//             }
+//         }
+//     },
+// render: function() {
+//     return(
+//         <h1>Oi {this.props.name + ' ' + this.props.lastname.a + this.props.lastname.b} </h1>
+//     )
+// }
+// })
+// 
 export default Title

647bbdc034fbb079aa660b687c314e1fc2ab2d4f
rhomennik <rhomerios2@gmail.com>
2019-03-26 13:45:55 -0400

    Fazendo request dos usuarios na API DO GITHUB (GITHUB APP)

--- code/m01/github-app/src/components/search.js
+++ code/m01/github-app/src/components/search.js
@@ -1,11 +1,27 @@
 'use strict'
 
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = () => (
     <div className='search'>
         <input type='search' 
-        placeholder='Digite o nome do usuario no Github'/>
+        placeholder='Digite o nome do usuario no Github'
+        onKeyUp={(e) => {
+            const value = e.target.value
+            const keyCode = e.which || e.keyCode
+            const ENTER = 13
+
+            if ( keyCode === ENTER ) {
+               ajax().get(`https://api.github.com/users/${value}`)
+               .then((result) => {
+                   console.log(result)
+               })
+            }
+            console.log(keyCode)
+        }}
+        
+        />
     </div>
 )
 

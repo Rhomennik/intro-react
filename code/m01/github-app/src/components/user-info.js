1664a45f0e6c2b6b97d64aab466bf346f5a85bac
rhomennik <rhomerios2@gmail.com>
2019-03-26 11:36:21 -0400

    Separando os componentes (GithubApp, Add props)

---
+++ code/m01/github-app/src/components/user-info.js
@@ -0,0 +1,19 @@
+'user strict'
+
+import React from 'react'
+
+const  UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars2.githubusercontent.com/u/24496303?v=4' />
+    <h1>
+       <a href='https://github.com/Rhomennik'>Rhomennik Firmino</a>
+       </h1>
+    <lu className='respos-info'>
+      <li>Repositorios: 28</li>
+      <li>Seguidores: 6</li>
+      <li>Seguindo: 1</li>
+   </lu>
+ </div>
+)
+
+export default UserInfo

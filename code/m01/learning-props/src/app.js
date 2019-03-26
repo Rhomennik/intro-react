8fbae0f4233f3c57c227d5d68cc35c3b8bc8a673
rhomennik <rhomerios2@gmail.com>
2019-03-26 08:37:54 -0400

    Formularios(checkbox e Radios)

--- code/m01/learning-props/src/app.js
+++ code/m01/learning-props/src/app.js
@@ -10,7 +10,9 @@ class App extends Component {
     constructor () {
         super()
         this.state = {
-            value: 'Valor Inicial'
+            value: 'Valor Inicial',
+            checked: false,
+            checked2: false
         }
     }
     render () {
@@ -23,8 +25,26 @@ class App extends Component {
                     value: e.target.value
                 })
             }} /> 
+            <label>
+                <input type='checkbox' value='Legal' checked={this.state.checked} onChange={(e) => {
+                    console.log(e.target.checked)
+                    this.setState({
+                        checked: !this.state.checked
+                    })
+                }}/>
+                Checkbox
+            </label>
+                
+            <label>
+                <input type='checkbox' value='Legal' defaultChecked />
+                Checkbox
+            </label>
+                radio
+            <input type='radio' name='rd' value='1' /> Radio 1
+            <input type='radio' name='rd' value='2' /> Radio 2
         </form>
     </div>
+                // Target: Meu input
         )
     }
 }

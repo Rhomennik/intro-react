dbfacb7046d49f8476bdab488f35ba71e47a94de
rhomennik <rhomerios2@gmail.com>
2019-04-03 09:07:19 -0400

    test(TDD): Finalizando testes

--- code/m02/jest-in-practice/src/map/map.js
+++ code/m02/jest-in-practice/src/map/map.js
@@ -1,6 +1,11 @@
 'use strict'
 const map = (arr = [], func= (item) => item) => {
-          
+    if(!Array.isArray(arr)) {
+        throw new TypeError('The first parameter should must be an array')
+    }
+    if(typeof func !== 'function') {
+        throw new TypeError('The second parameter must be a function')
+    }
 let newArr = []
         for(let i = 0; i < arr.length; i++) 
             newArr.push(func(arr[i], i, arr))

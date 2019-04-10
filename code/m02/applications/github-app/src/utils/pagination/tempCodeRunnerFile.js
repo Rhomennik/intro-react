31d81974c57ce9fc7dc5306500181ed549bc2cf7
rhomennik <rhomerios2@gmail.com>
2019-04-10 15:42:19 -0400

    feat(Imp): IMplementacao Part-2

--- code/m02/applications/github-app/src/utils/pagination/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const pagination = ({ total, activePage }) => {
-  let pages = []
-  for (let i = 0; i < total; i++) {
-    pages.push(i + 1)
-  }
-  return pages
-}
-console.log(pagination(100, 1))

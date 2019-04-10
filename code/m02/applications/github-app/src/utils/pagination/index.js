b26f0af9184fb09bf026089bf6cd436c55e1955d
rhomennik <rhomerios2@gmail.com>
2019-04-10 16:05:03 -0400

    feat(IMp): implementacao part 3

--- code/m02/applications/github-app/src/utils/pagination/index.js
+++ code/m02/applications/github-app/src/utils/pagination/index.js
@@ -1,8 +1,24 @@
 'use strict'
+const centerRule = ({ total, activePage }) => {
+  if (activePage - 1 <= 0){
+    return 1
+  }
+  return activePage - 1
+}
 const pagination = ({ total, activePage }) => {
   if (total <= 5) {
     return Array.from({ length: total }, (_, i) => i + 1)
   }
-  return [1, 2, 3, '...', 6]
+
+  const visiblePages = 3
+  let pages = [
+    1,
+    ...Array.from({ length: visiblePages }, (_, i) => i + centerRule({ total, activePage})),
+    total
+  ]
+
+  pages = pages.filter((pages, index, array) => array.indexOf(pages) === index)
+
+  return pages
 }
 export default pagination

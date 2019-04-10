c7bacc7983aecbfb3127cff22455e5237e1997ca
rhomennik <rhomerios2@gmail.com>
2019-04-10 13:47:06 -0400

    feat(config): configurando ambientes para testes-linter

---
+++ code/m02/applications/github-app/src/components/repos/repos.story.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import { storiesOf } from '@kadira/storybook'
+import React from 'react'
+import Repos from './index'
+storiesOf('Repos', module)
+  .add('with title prop', () => (
+    <Repos title='Favoritos' />
+))
+
+.add('with repos', () => (
+  <div style={{padding: '50px'}}>
+    <Repos
+      title=''
+      repos={[{
+        link: 'http://blog.da2k.com.br',
+        name: 'Blog da2k'
+      }]}
+     />
+  </div>
+))

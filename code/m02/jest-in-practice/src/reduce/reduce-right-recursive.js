2b597257ad144af4c6eed323bd86e24c2e45eb69
rhomennik <rhomerios2@gmail.com>
2019-06-26 14:55:17 -0400

    fix bug import es6

--- code/m02/jest-in-practice/src/reduce/reduce-right-recursive.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-import mainReduceRecursive from './main-reduce-recursive';
-import reverse from '../reverse/reverse'
-
-const reduceRight = (arr, ...params) => mainReduceRecursive(reverse(arr), ...params) 
-
-export default reduceRight
